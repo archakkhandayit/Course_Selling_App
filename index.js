9e6f32c682095252f50fff532701ab4c7d123356
archakkhandayit <168993192+archakkhandayit@users.noreply.github.com>
2025-02-14 02:02:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/", () => {
+app.get("/", (req, res) => {
     res.status(200).json(
         "App is running"
     );
